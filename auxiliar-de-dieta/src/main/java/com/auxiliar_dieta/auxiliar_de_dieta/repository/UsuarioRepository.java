786eeca34c1b895af65079a842ac73a82c1bf632
Daniel Ribeiro Alcântara Marques <126820254+Dr3002@users.noreply.github.com>
2024-09-26 23:32:28 -0300

    Commit 5
    Criação da tela de receitas, refeições e da filtragem de receitas

--- auxiliar-de-dieta/src/main/java/com/auxiliar_dieta/auxiliar_de_dieta/repository/UsuarioRepository.java
+++ auxiliar-de-dieta/src/main/java/com/auxiliar_dieta/auxiliar_de_dieta/repository/UsuarioRepository.java
@@ -4,5 +4,4 @@ import com.auxiliar_dieta.auxiliar_de_dieta.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    // Metodos de Consulta
 }
